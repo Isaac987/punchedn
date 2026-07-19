63632677958cae95a51208fcb4c6246703f64f9b
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-19 07:01:38 -0700

    Added callback component following setup guide

---
+++ frontend/src/CallBack.tsx
@@ -0,0 +1,18 @@
+import { useHandleSignInCallback } from "@logto/react";
+import { useNavigate } from "react-router";
+
+export const Callback = () => {
+  const navigate = useNavigate();
+
+  const { isLoading } = useHandleSignInCallback(() => {
+    // Navigate to root path when finished
+    navigate("/");
+  });
+
+  // When it's working in progress
+  if (isLoading) {
+    return <div>Redirecting...</div>;
+  }
+
+  return null;
+};
