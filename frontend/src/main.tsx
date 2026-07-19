46fc976a2fcfbe595537fafa872c7d38b7bcded5
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-19 07:01:59 -0700

    Setup basic Logto provider and route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { Callback } from "./CallBack.tsx";
+import { LogtoProvider, type LogtoConfig } from "@logto/react";
+import { LOGTO_SCOPES } from "./logtoScopes.ts";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+  {
+    path: "callback",
+    Component: Callback,
+  },
+]);
+
+const config: LogtoConfig = {
+  endpoint: "https://y9fivj.logto.app/",
+  appId: "994bq29s02bwqwawpg7yl",
+  resources: ["https://dev.punchedn.com/api"],
+  scopes: LOGTO_SCOPES,
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LogtoProvider config={config}>
+      <RouterProvider router={router} />,
+    </LogtoProvider>
   </StrictMode>,
-)
+);
