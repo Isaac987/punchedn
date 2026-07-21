1cd8478e07f6507678918746e24471924e4d5088
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-20 22:54:48 -0700

    Added a mock schema

---
+++ backend/app/features/users/schemas.py
@@ -0,0 +1,28 @@
+from datetime import date, datetime
+from typing import List, Optional
+
+from beanie import PydanticObjectId
+from pydantic import BaseModel, EmailStr, Field
+from pydantic_extra_types.phone_numbers import PhoneNumber
+
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: PhoneNumber
+
+
+class UserRead(UserBase):
+    id: PydanticObjectId = Field(alias="_id")
+    roles: List[str] = []
+    is_active: bool
+    created_at: datetime
+
+
+class UserCreate(UserBase): ...
+
+
+# This is different from the rest
+# A patch may only update one field
+class UserUpdate(BaseModel): ...
