a86336f9855511e5a2d0c411e534ba4d70759984
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-20 22:53:22 -0700

    Added created at field and changed the collection name

--- backend/app/features/users/models.py
+++ backend/app/features/users/models.py
@@ -2,14 +2,19 @@ from datetime import datetime
 from typing import Annotated, Optional
 
 from beanie import Document, Indexed  # type: ignore
+from pydantic import Field
 
 
 class UserModel(Document):
     auth_id: Annotated[str, Indexed(unique=True)]
     first_name: str
     last_name: str
-    email: str
+    email: Annotated[str, Indexed(unique=True)]
     phone_number: str
     role: Optional[str] = None
     hire_date: Optional[datetime] = None
     is_active: bool
+    created_at: datetime = Field(default_factory=datetime.now)
+
+    class Settings:
+        name = "users"
