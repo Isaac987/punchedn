a297517a59353cdc734eb5f116f3b7e786d87753
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-21 21:22:10 -0700

    Added function description and changed logto_id to auth_id if we want to switch in the future.

--- backend/app/features/users/dependencies.py
+++ backend/app/features/users/dependencies.py
@@ -3,8 +3,8 @@ from typing import Annotated, Any, Dict
 from core.security import verify_jwt
 from fastapi import Depends, HTTPException, Security, status
 
+from .models import UserModel
 from .repository import UserRepository
-from .schemas import UserRead
 
 
 async def get_current_user(
@@ -13,17 +13,20 @@ async def get_current_user(
         Security(verify_jwt),
     ],
     repository: Annotated[UserRepository, Depends(UserRepository)],
-) -> UserRead:
+) -> UserModel:
+    """
+    Validates the JWT token, enforces permission scopes, and returns the UserModel.
+    """
     # Use get rather than [] for missing key validation
     # TODO: Create a payload object and validate members before getting to this point
-    logto_id = payload.get("sub")
+    auth_id = payload.get("sub")
 
-    if not logto_id:
+    if not auth_id:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Token missing subject claim",
         )
 
-    user_model = await repository.get_by_auth_id(logto_id)
+    user_model = await repository.get_by_auth_id(auth_id)
 
-    return UserRead.model_validate(user_model)
+    return user_model
