c7edc5c4019ef5c8c8f5e5a3950510976a52337a
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-23 22:07:09 -0700

    Added logto client and jwt auth classes

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,7 @@
-from typing import Annotated, Any, Dict
+import time
+from typing import Annotated, Any, Dict, Generator
 
+import httpx
 import jwt
 import structlog
 from fastapi import Depends, HTTPException, status
@@ -11,6 +13,137 @@ from core.config import get_settings
 _logger = structlog.get_logger()
 _settings = get_settings()
 _security = HTTPBearer()
+
+
+class BearerAuth(httpx.Auth):
+    """
+    A httpx authentication scheme for interfacing with Logto API.
+    """
+
+    def __init__(self, token: str):
+        self._token = token
+
+    def auth_flow(
+        self, request: httpx.Request
+    ) -> Generator[httpx.Request, httpx.Response, None]:
+        request.headers["authorization"] = f"Bearer {self._token}"
+        yield request
+
+
+class LogtoAPIClient:
+    def __init__(
+        self,
+        logto_uri: str,
+        app_id: str,
+        app_secret: str,
+        token_expiration_threshold: int = 10,
+    ) -> None:
+        self._logto_uri = logto_uri
+        self._logto_api_uri = f"{logto_uri.rstrip('/')}/api"
+
+        self._token: str | None = None
+        self._token_expiration: float = 0
+        self._token_expiration_threshold: float = token_expiration_threshold
+
+        self._token_auth = httpx.BasicAuth(app_id, app_secret)
+        self._api_auth: BearerAuth
+
+        self._client: httpx.AsyncClient
+
+    def connect(self):
+        self._client = httpx.AsyncClient(
+            base_url=self._logto_api_uri,
+        )
+
+    async def disconnect(self):
+        await self._client.aclose()
+
+    async def create_user(
+        self,
+        username: str,
+        email: str,
+        first_name: str,
+        last_name: str,
+    ) -> str:
+        """
+        Creates new Logto user and returns their Logto ID.
+        Ref: https://openapi.logto.io/operation/operation-createuser
+        """
+        auth = BearerAuth(await self._get_access_token())
+        response = await self._client.post(
+            "/users",
+            auth=auth,
+            json={
+                "primaryEmail": email,
+                "username": username,
+                "profile": {"givenName": first_name, "familyName": last_name},
+            },
+        )
+
+        if response.status_code != 200:
+            _logger.error(
+                "logto_m2m_create_user_failed",
+                response_status_code=response.status_code,
+                response_text=response.text,
+            )
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to create user with the identity provider.",
+            )
+
+        return response.json()["id"]
+
+    # TODO: Define Response
+    async def get_user(self, logto_id: str): ...
+
+    async def _get_access_token(self) -> str:
+
+        # Early return if the token exists and does not expire within the threshold time
+        if self._token and self._token_expiration > (
+            time.time() - self._token_expiration_threshold
+        ):
+            return self._token
+
+        _logger.info("logto_fetching_new_token")
+
+        data = {
+            "grant_type": "client_credentials",
+            "resource": self._logto_api_uri,
+            "scope": "all",
+        }
+
+        # Get the access token
+        # Ref: https://docs.logto.io/integrate-logto/interact-with-management-api#typical-scenarios-for-using-logto-management-api
+        response = await self._client.post(
+            f"{self._logto_uri.rstrip('/')}/oidc/token",
+            auth=self._token_auth,
+            data=data,
+        )
+
+        # TODO: Use a secondary app secret? Try again?
+        if response.status_code != 200:
+            _logger.error(
+                "logto_m2m_auth_failed",
+                data=data,
+                response_status_code=response.status_code,
+                response_text=response.text,
+            )
+
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to authenticate with the identity provider.",
+            )
+
+        # Here is our token xD
+        token = response.json()["access_token"]
+
+        # Populate the token expiration time
+        # We don't need to validate the signature since we are the user
+        self._token_expiration = jwt.decode(token, options={"verify_signature": False})[
+            "exp"
+        ]
+
+        return token
 
 
 class LogtoAuthenticator:
@@ -75,6 +208,17 @@ class LogtoAuthenticator:
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail=f"Invalid token: {str(e)}",
             )
+
+
+_logto_api_client = LogtoAPIClient(
+    str(_settings.logto_uri),
+    _settings.logto_admin_app_id,
+    _settings.logto_admin_app_secret.get_secret_value(),
+)
+
+
+def get_logto_api_client() -> LogtoAPIClient:
+    return _logto_api_client
 
 
 verify_jwt = LogtoAuthenticator(
