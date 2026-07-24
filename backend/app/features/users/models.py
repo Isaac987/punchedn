bb5410259a726df2540be4cf6c248b89d558e8ff
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-23 22:07:49 -0700

    Added some user fields and a index by first and last name

--- backend/app/features/users/models.py
+++ backend/app/features/users/models.py
@@ -1,20 +1,25 @@
 from datetime import datetime
 from typing import Annotated, Optional
 
+import pymongo
 from beanie import Document, Indexed  # type: ignore
 from pydantic import Field
 
 
 class UserModel(Document):
     auth_id: Annotated[str, Indexed(unique=True)]
+    username: Annotated[str, Indexed(unique=True)]
+    email: Annotated[str, Indexed(unique=True)]
     first_name: str
     last_name: str
-    email: Annotated[str, Indexed(unique=True)]
     phone_number: str
     role: Optional[str] = None
     hire_date: Optional[datetime] = None
-    is_active: bool
+    is_active: bool = True
     created_at: datetime = Field(default_factory=datetime.now)
 
     class Settings:
         name = "users"
+        indexes = [
+            [("first_name", pymongo.ASCENDING), ("last_name", pymongo.ASCENDING)]
+        ]
