500ab4583012a6847fdad7519b981213e5af89bc
Isaac Perkins <isaacperkins987@gmail.com>
2026-07-23 22:09:41 -0700

    Added username, and made it easier to copy a document to the model using from_attributes and populate by name

--- backend/app/features/users/schemas.py
+++ backend/app/features/users/schemas.py
@@ -1,5 +1,5 @@
-from datetime import date, datetime
-from typing import List, Optional
+from datetime import datetime
+from typing import List
 
 from beanie import PydanticObjectId
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
@@ -9,16 +9,18 @@ from pydantic_extra_types.phone_numbers import PhoneNumber
 class UserBase(BaseModel):
     first_name: str
     last_name: str
+    username: str
     email: EmailStr
     phone_number: PhoneNumber
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
 
 
-class UserRead(UserBase):
+class UserResponse(UserBase):
     id: PydanticObjectId = Field(alias="_id")
     roles: List[str] = []
     is_active: bool
     created_at: datetime
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
 
 
 class UserCreate(UserBase): ...
